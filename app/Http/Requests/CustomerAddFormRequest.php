00a66df7e20e64d978b9649ec8c2bc95f57fe389
نورهان طاهر <norhan@lacasacode.come>
2022-06-28 11:54:11 +0200

    update

--- app/Http/Requests/CustomerAddFormRequest.php
+++ app/Http/Requests/CustomerAddFormRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CustomerAdd extends FormRequest
+class CustomerAddFormRequest extends FormRequest
 {
     
     public function authorize()
