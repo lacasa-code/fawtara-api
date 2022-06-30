b220463385729cb227d5df10e1d4971a2d57084e
نورهان طاهر <norhan@lacasacode.come>
2022-06-30 12:46:59 +0200

    update

--- app/Models/Electronicinvoice.php
+++ app/Models/Electronicinvoice.php
@@ -10,9 +10,44 @@ class Electronicinvoice extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'Invoice_type',
+        'Invoice_Number',
+        'Customer',
+        'customer_address',
+        'customer_vat',
+        'customer_phone',
+        'customer_number',
+        'Job_card',
+        'quotation_number',
+        'customer_po_number',
+        'branch_name',
+        'meters_reading',
+        'fleet_number',
+        'registeration',
+        'manufacturer',
+        'chassis_no',
+        'model_name',
+        'vehicle',
+        'Date',
+        'Discount',
+        'job_open_date',
+        'delivery_date',
+        'Details',
+        'vat_number',
+        'Status',
+        'Payment_type',
+        'services_sum',
+
+        'total_amount',
+        'grand_total',
+        'tax',
+        'paid_amount',
+        'branch_id',
+
+        'final',
+        'reg_chars',
+        'final',
+        'customer_id'
     ];
 
     protected $hidden = [
@@ -24,5 +59,15 @@ class Electronicinvoice extends Model
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function services()
+    {
+        return $this->hasMany(Invoiceservice::class, 'invoice_id', 'id');
+    }
+
+     public function branch()
+    {
+        return $this->belongsTo(Branch::class, 'branch_id');
+    }
 
 }
