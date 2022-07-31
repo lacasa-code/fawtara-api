c42e559fbd94065ef5d6ae5123b42036491f2264
نورهان طاهر <norhan@lacasacode.come>
2022-07-31 11:40:56 +0200

    update

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -39,15 +39,15 @@ class ReportController extends Controller
 
         //$end = Carbon::parse($request->end_date)->toDateTimeString();
 
-        $start =  Carbon::parse($request->start_date)->startOfDay()->format('Y-m-d');
-        $end = Carbon::parse($request->end_date)->endOfDay()->format('Y-m-d');
+        $start =  Carbon::parse($request->start_date)->startOfDay()->format('Y-m-d H:i:s');
+        $end = Carbon::parse($request->end_date)->endOfDay()->format('Y-m-d H:i:s');
 
         $Total_Invoices = Electronicinvoice::where(['branch_id' => auth()->user()->branch_id,'final' => 1,'deleted_at' => NULL])->whereBetween('created_at', [$start, $end])->count();
         $page_size = $request->page_size ?? 10 ;
 
         $invoices=Electronicinvoice::where('branch_id',auth()->user()->branch_id)->where('final',1)->whereNull('deleted_at')->whereBetween('created_at', [$start, $end])->orderBy('id','DESC')->paginate($page_size);
         $total_amounts =Electronicinvoice::select(DB::raw('SUM(paid_amount) as total_paid_amount'))->where('branch_id',auth()->user()->branch_id)->where('final',1)->whereNull('deleted_at')->whereBetween('created_at', [$start, $end])->get();   
-        $total_amounts_without_tax =Electronicinvoice::select(DB::raw('SUM(total_amount) as total_without_tax_amount'))->where('branch_id',auth()->user()->branch_id)->where('final',1)->whereNull('deleted_at')->whereBetween('created_at', [$start, $end])->get();   
+        $total_amounts_without_tax =Electronicinvoice::select(DB::raw('SUM(total_amount) as total_without_tax_amount'))->where('branch_id',auth()->user()->branch_id)->where('final',1)->whereNull('deleted_at')->get();   
 
         return response()->json([
             'status'=>true,
